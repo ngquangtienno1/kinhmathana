591d4413831696369a18214668278ac8bbb446ca
anhntgxe <tanh.gxe@gmail.com>
2025-05-20 11:58:04 +0700

    update brand

--- database/factories/BrandFactory.php
+++ database/factories/BrandFactory.php
@@ -15,7 +15,6 @@ class BrandFactory extends Factory
         return [
             'name' => $this->faker->company,
             'description' => $this->faker->catchPhrase,
-            'logo_path' => $this->faker->imageUrl(200, 100, 'business'),
             'image' => null,
             'is_active' => $this->faker->boolean(80),
             'created_at' => now(),
