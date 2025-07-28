5f6702441641e6230a9ca63b88451528e82e9472
DevCungAnh <anhntph51526@gmail.com>
2025-07-28 18:43:08 +0700

    update: seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,61 +14,40 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // // Tạo tài khoản admin mặc định
-        // User::create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@example.com',
-        //     'password' => Hash::make('password'),
-        //     'address' => '123 Admin Street',
-        //     'phone' => '0123456789',
-        //     'date_birth' => '1990-01-01',
-        //     'gender' => 'male',
-        //     'status_user' => 'active',
-        //     'role_id' => 1, // Admin role
-        //     'email_verified_at' => now(),
-        //     'phone_verified_at' => now(),
-        // ]);
+        // Tạo tài khoản admin mặc định
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'address' => '123 Admin Street',
+            'phone' => '0123456789',
+            'date_birth' => '1990-01-01',
+            'gender' => 'male',
+            'status_user' => 'active',
+            'role_id' => 1, // Admin role
+            'email_verified_at' => now(),
+            'phone_verified_at' => now(),
+        ]);
 
-        // // Tạo tài khoản nhân viên mặc định
-        // User::create([
-        //     'name' => 'Staff',
-        //     'email' => 'staff@example.com',
-        //     'password' => Hash::make('password'),
-        //     'address' => '456 Staff Street',
-        //     'phone' => '0987654321',
-        //     'date_birth' => '1995-01-01',
-        //     'gender' => 'female',
-        //     'status_user' => 'active',
-        //     'role_id' => 2, // Staff role
-        //     'email_verified_at' => now(),
-        //     'phone_verified_at' => now(),
-        // ]);
+        // Tạo tài khoản nhân viên mặc định
+        User::create([
+            'name' => 'Staff',
+            'email' => 'staff@example.com',
+            'password' => Hash::make('password'),
+            'address' => '456 Staff Street',
+            'phone' => '0987654321',
+            'date_birth' => '1995-01-01',
+            'gender' => 'female',
+            'status_user' => 'active',
+            'role_id' => 2, // Staff role
+            'email_verified_at' => now(),
+            'phone_verified_at' => now(),
+        ]);
 
-        // // Tạo tài khoản khách hàng mặc định
-        // User::create([
-        //     'name' => 'Customer',
-        //     'email' => 'ajast@example.org',
-        //     'password' => Hash::make('password'),
-        //     'address' => '456 Staff Street',
-        //     'phone' => '0987654321',
-        //     'date_birth' => '1995-01-01',
-        //     'gender' => 'female',
-        //     'status_user' => 'active',
-        //     'role_id' => 3, // Staff role
-        //     'email_verified_at' => now(),
-        //     'phone_verified_at' => now(),
-        // ]);
-
-        // // Tạo thêm 20 tài khoản ngẫu nhiên
-        // User::factory()->count(10)->create([
-        //     'role_id' => 3, // Customer role
-        //     'status_user' => 'active',
-        // ]);
-
-         // Tạo tài khoản khách hàng mặc định
-         User::create([
-            'name' => 'Quang TiếnTiến',
-            'email' => 'tiennqph51552@gmail.comcom',
+        // Tạo tài khoản khách hàng mặc định
+        User::create([
+            'name' => 'Customer',
+            'email' => 'ajast@example.org',
             'password' => Hash::make('password'),
             'address' => '456 Staff Street',
             'phone' => '0987654321',
@@ -78,6 +57,12 @@ class UserSeeder extends Seeder
             'role_id' => 3, // Staff role
             'email_verified_at' => now(),
             'phone_verified_at' => now(),
+        ]);
+
+        // Tạo thêm 20 tài khoản ngẫu nhiên
+        User::factory()->count(10)->create([
+            'role_id' => 3, // Customer role
+            'status_user' => 'active',
         ]);
     }
 }
